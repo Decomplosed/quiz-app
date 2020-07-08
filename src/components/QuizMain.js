b1b789a7448eea3802ef86fe72a7250378beaea7
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-08 08:14:47 +0200

    created basic class component template

---
+++ src/components/QuizMain.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class QuizMain extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default QuizMain
