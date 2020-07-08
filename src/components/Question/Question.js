d63a4be566c42f411656fb9ca1b5e8c942cf9c4f
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-08 08:29:57 +0200

    added some dummy text to see if it renders

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,8 @@
 import React from 'react'
+import './Question.css'
 
 const Question = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <h1>Question</h1>
 }
 
 export default Question
