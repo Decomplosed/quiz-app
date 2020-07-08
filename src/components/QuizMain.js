378e4915fbc538027d9fda6f205899173c974aad
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-08 08:15:09 +0200

    imported styles

--- src/components/QuizMain.js
+++ src/components/QuizMain.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
+import './QuizMain.css'
 
 export class QuizMain extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div></div>
   }
 }
 
