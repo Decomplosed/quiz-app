e6cbdbd0c683a7f8fb8388387d6869aa198dc2f3
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-08 08:47:53 +0200

    destructured answers from props

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Answer.css'
 
-const Answer = (props) => {
-  let answers = Object.keys(props.answers).map((answer, i) => (
-    <li key={answer}>{props.answers[answer]}</li>
+const Answer = ({ answers }) => {
+  let answersMarkup = Object.keys(answers).map((answer, i) => (
+    <li key={answer}>{answers[answer]}</li>
   ))
 
-  return <ul className='Answers'>{answers}</ul>
+  return <ul className='Answers'>{answersMarkup}</ul>
 }
 
 export default Answer
