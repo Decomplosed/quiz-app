2c643ee7b1a26b5788ad319cf19dc51a60d8fd95
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-09 14:09:53 +0200

    extracted checkAnswer from props

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Answer.css'
 
-const Answer = ({ answers }) => {
+const Answer = ({ answers, checkAnswer }) => {
   let answersMarkup = Object.keys(answers).map((answer, i) => (
     <li key={answer}>{answers[answer]}</li>
   ))
