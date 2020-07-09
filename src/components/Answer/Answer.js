1ffc0aaab844bf32d42ceba33f56a08033b79efc
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-09 14:27:00 +0200

    added logic to show the right classname

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -3,7 +3,17 @@ import './Answer.css'
 
 const Answer = ({ answers, checkAnswer, correctAnswer, clickedAnswer }) => {
   let answersMarkup = Object.keys(answers).map((answer, i) => (
-    <li key={answer} onClick={() => checkAnswer(answer)}>
+    <li
+      key={answer}
+      className={
+        correctAnswer === answer
+          ? 'correct'
+          : clickedAnswer === answer
+          ? 'incorrect'
+          : ''
+      }
+      onClick={() => checkAnswer(answer)}
+    >
       {answers[answer]}
     </li>
   ))
