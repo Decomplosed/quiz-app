a5dd7cd3197c2b2cd8a96510c56df65d84f3ce9c
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-09 14:10:07 +0200

    added checkAnswer under onCLick event

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -3,7 +3,9 @@ import './Answer.css'
 
 const Answer = ({ answers, checkAnswer }) => {
   let answersMarkup = Object.keys(answers).map((answer, i) => (
-    <li key={answer}>{answers[answer]}</li>
+    <li key={answer} onClick={() => checkAnswer(answer)}>
+      {answers[answer]}
+    </li>
   ))
 
   return <ul className='Answers'>{answersMarkup}</ul>
